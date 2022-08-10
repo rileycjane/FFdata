6fd989c0e4b10682724a9a069eda6f3b3648130a
Riley Coad <77368857+rileycjane@users.noreply.github.com>
2022-08-09 22:45:17 -0500

    "removing comments from jupyter notebook and adding getLeague routine"

--- api_setup.py
+++ api_setup.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 from espn_api.football import League
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import json
 
-
-# Importing league data
-
-# In[2]:
-
-
-league_id = 49765217
-year = 2021
-swid='{771D2F0F-5C54-4553-8582-FCC07D235F88}'
-espn_s2='AEAFCR%2F9i%2Bb3h4hoHm9sjfpXH0lhBDKmfgKEV8nU5aAocybEqSM9tlUJ1CpJtgcBN9ZKLgZM42hmCLMYICupuwIUpkZaBnDhdd886U9o8a1%2F9Sml5Vn0kSRvVGUGblNLTuxoFwBkwPyzY%2FKqdLtnqXD3DVKtATcN4Lu2VnleVW6soxz6GP7m84wgUIo5cF6b%2FcDRgg8uE8XMVmeacZGwLlouk5pdsy19MQSRjsTXcBvxZdkaA0PdId4RnS16s9Gsbbo77BdqrB4QWbiQqOx4SP4jrseK%2FVYBTDLSPmdsnVlOBw%3D%3D'
-league2021 = League(league_id,year,espn_s2,swid)
-
+def getLeague(id,year,swid,s2):
+    try:
+        return League(id,year,swid,s2)
+    except:
+        return
 
 # Importing ADP data for 2021 and defining functionality to search by name
-
-# In[3]:
-
-
 with open("ADPdata2021.json") as f:
     adp=json.load(f)
     
@@ -37,21 +23,6 @@ def searchADPByName(name):
     except:
         return
     return result
-
-
-# attempting to collect some data and plot
-
-# In[8]:
-
-
-teamCoad = league2021.teams[15]
-teams = []
-for team in league2021.teams:
-    teams.append(team.team_abbrev)
-
-
-# In[9]:
-
 
 def draftByTeam(xLeague,xTeam):
     xDraft = []
@@ -68,33 +39,26 @@ def draftByTeam(xLeague,xTeam):
             xDraft.append(xPick)
     return xDraft
 
+###### executable code below
 
-# In[10]:
+# Importing league data
+league_id = 49765217
+year = 2021
+swid='{771D2F0F-5C54-4553-8582-FCC07D235F88}'
+espn_s2='AEAFCR%2F9i%2Bb3h4hoHm9sjfpXH0lhBDKmfgKEV8nU5aAocybEqSM9tlUJ1CpJtgcBN9ZKLgZM42hmCLMYICupuwIUpkZaBnDhdd886U9o8a1%2F9Sml5Vn0kSRvVGUGblNLTuxoFwBkwPyzY%2FKqdLtnqXD3DVKtATcN4Lu2VnleVW6soxz6GP7m84wgUIo5cF6b%2FcDRgg8uE8XMVmeacZGwLlouk5pdsy19MQSRjsTXcBvxZdkaA0PdId4RnS16s9Gsbbo77BdqrB4QWbiQqOx4SP4jrseK%2FVYBTDLSPmdsnVlOBw%3D%3D'
+#league2021 = League(league_id,year,espn_s2,swid)
+league2021 = getLeague(league_id,year,swid,espn_s2)
 
+teamCoad = league2021.teams[15]
+teams = []
+for team in league2021.teams:
+    teams.append(team.team_abbrev)
 
 myDraft = draftByTeam(league2021,teamCoad)
 
 
-# In[11]:
-
-
-myDraft
-
-
-# In[ ]:
 
 
 
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
+print(myDraft)
 
